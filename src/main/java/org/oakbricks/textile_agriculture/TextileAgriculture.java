c46b942a355418394d4f79d5cbb335213c6b242f
OakBricks <oakbricks@gmail.com>
2021-06-13 23:09:51 -0500

    e

--- src/main/java/org/oakbricks/textile_agriculture/TextileAgriculture.java
+++ src/main/java/org/oakbricks/textile_agriculture/TextileAgriculture.java
@@ -1,14 +1,16 @@
-package net.fabricmc.example;
+package org.oakbricks.textile_agriculture;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class TextileAgriculture implements ModInitializer {
+	public static final String MOD_ID = "textile_agriculture";
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		System.out.println("Hello Fabric world!");
+		System.out.println("");
 	}
 }
---
+++ src/main/java/org/oakbricks/textile_agriculture/items/TAItems.java
@@ -0,0 +1,13 @@
+package org.oakbricks.textile_agriculture.items;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+
+public class TAItems {
+    public static final Item AVOCADO = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+
+    public static void registerTAItems() {
+
+    }
+}
