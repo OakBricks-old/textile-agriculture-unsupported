05a7fed14a018dcd983d7eb327f7cd4212780ec6
OakBricks <oakbricks@gmail.com>
2021-06-15 02:41:26 -0500

    big update, mod works now, just need to add lang files and more crops

--- src/main/java/org/oakbricks/textile_agriculture/TextileAgricultureClient.java
+++ src/main/java/org/oakbricks/textile_agriculture/TextileAgricultureClient.java
@@ -2,6 +2,9 @@ package org.oakbricks.textile_agriculture;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
+import net.minecraft.client.color.world.BiomeColors;
+import net.minecraft.client.color.world.FoliageColors;
 import net.minecraft.client.render.RenderLayer;
 import org.oakbricks.textile_agriculture.blocks.TABlocks;
 
@@ -13,5 +16,9 @@ public class TextileAgricultureClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         BlockRenderLayerMap.INSTANCE.putBlock(TABlocks.AVOCADO_PLANT, RenderLayer.getCutout());
+        ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) ->
+                view != null && pos != null
+                    ? BiomeColors.getFoliageColor(view, pos)
+                    : FoliageColors.getDefaultColor(), TABlocks.AVOCADO_PLANT);
     }
 }
